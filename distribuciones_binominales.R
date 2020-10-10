488cfc9a54bca14af46b373030b7f4b5014e18de
YosefGuevara012 <yosef.guevara012@hotmail.com>
2020-10-10 17:14:26 -0500

    Se terminan ejercicios de distribucion

--- distribuciones_binominales.R
+++ distribuciones_binominales.R
@@ -17,7 +17,8 @@ b <- 1 - sum(res[1:8]);b
 c <- 1-res[10];c
 
 #5.24
-
+# this exercies i'm not sure the question does no fit with the values
+# on the book
 
 n <- 125
 p <- 0.1
@@ -56,3 +57,55 @@ res <- dbinom(x,n,p);res
 barplot(res, names.arg = x)
 
 f <- sum(res[21:125]);f
+
+#5.27
+
+n <- 4
+p <- 0.3
+x <- 0:4
+
+res <- dbinom(x,n,p);res
+barplot(res, names.arg = x)
+
+#a p(X = 4)
+
+a <- res[5];a
+
+#b p(X = 1)
+
+b <- res[2];b
+
+# ninguna tendra que olvidarse
+
+c <- res[1];c
+
+# 5.29 
+
+n <- 100
+p <- 0.1
+x <- 0:100
+
+res <- dbinom(x,n,p);res
+barplot(res, names.arg = x)
+
+#a 
+u <- n*p;u
+
+#b
+
+sd <- sqrt(n*p*(1-p));sd
+
+
+# u +/- (2sd)
+
+# despenjando
+
+ys <- u + 2*sd;ys
+yi <- u - 2*sd;yi
+
+limites <- sum(res[yi+1:ys+1]);limites
+
+#C P(x = 25)
+
+c <- res[26];c
+
