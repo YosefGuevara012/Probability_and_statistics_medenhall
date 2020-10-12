b737ec29f0a5141fb1f6433b972384a195910dcb
YosefGuevara012 <yosef.guevara012@hotmail.com>
2020-10-12 13:39:20 -0500

    Taller 2 finished

--- Taller2.R
+++ Taller2.R
@@ -27,6 +27,19 @@ prob <- pbinom(x,n,p);prob
 
 d <- 1 - sum(dis[1:4]);d
 
+#3
+
+n <- 10
+p <- 4/20;p
+x <- 0:10
+
+dis <- dbinom(x,n,p);dis
+barplot(dis, names.arg = x)
+prob <- pbinom(x,n,p);prob
+barplot(prob, names.arg = x)
+
+res <- prob[7];res
+sum(dis[1:8])
 
 #4
 
@@ -52,3 +65,78 @@ b <- 1 - sum(dis[1:15]);b
 
 c <- sum(dis[1:11]);c
 
+#5
+
+u = 700/50
+x = 0:60
+dis <- dpois(x, u);dis
+barplot(dis,names.arg = x)
+
+#a
+res <- ppois(10, u);res
+
+#b
+
+res <- ppois(10, u)^3;res
+
+#6
+
+u <- 6
+
+x <- 0:20
+
+dis <- dpois(x, u);
+
+barplot(dis,names.arg = x)
+
+
+sd <- sqrt(u)
+
+li <- u - sd;li
+ls <- u + sd;ls
+
+res <- ppois(ls, u)-ppois(li, u);res
+
+#7
+
+x<-seq(-4,4, by = 0.01)
+u <- 0
+sd <- 1
+dis_norm <-  pnorm(x,mean=u,sd=sd);dis_norm
+
+#a P(z > 2)
+# 1- P(< 2)
+a <-  1 - pnorm(2,mean=u,sd=sd);a
+
+#b P(-2 < z < 2)
+
+b <-  pnorm(2,mean=u,sd=sd)- pnorm(-2,mean=u,sd=sd);b
+
+#c P(z <= 6)
+
+c <-  pnorm(6,mean=u,sd=sd);c
+
+#d P(0<= z <= 400)
+
+d <-  pnorm(6,mean=u,sd=sd)- pnorm(0,mean=u,sd=sd);d
+
+# 8
+x<-seq(-4,4, by = 0.01)
+u <- 163
+sd <- 9.68
+dis <- pnorm(x,mean=u,sd=sd);dis
+barplot(dis, names.arg = x)
+#b
+
+#10
+
+n <- 20
+p <- 0.4
+x <- 0:20
+
+dis <- dbinom(x,n,p);dis
+prob <-pbinom(x,n,p);prob
+
+barplot(dis, names.arg = x)
+barplot(prob, names.arg = x)
+res <- 1- pbinom(11,n,p);res
